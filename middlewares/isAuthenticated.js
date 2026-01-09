7fbd6e6b42c94c8413342066cc795776ec0fc41a
Yash Srivastava <133089351+Guru-yash@users.noreply.github.com>
2026-01-09 13:44:13 +0530

    Change JWT secret environment variable to SECRET_KEY

--- middlewares/isAuthenticated.js
+++ middlewares/isAuthenticated.js
@@ -11,7 +11,7 @@ const isAuthenticated = (req, res, next) => {
       });
     }
 
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const decoded = jwt.verify(token, process.env.SECRET_KEY);
     req.id = decoded.userId;
     next();
   } catch (error) {
